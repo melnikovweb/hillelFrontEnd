cdda98849c98b772f94fc4484528cc6a992478f8
Andrew Melnikov <melnikovweb@gmail.com>
2021-09-12 02:14:09 +0300

    almost done

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-alert(`test`);
-prompt(`proverka`)
+firstName = prompt("Enter first name pls").trim();
+lastName = prompt("Enter last name pls").trim();
+customerEmail = prompt("Enter email pls").replaceAll(' ','').toLowerCase();
+birth = prompt("Enter year of birth pls").replaceAll(' ','');
+date = new Date();
+age = date.getFullYear() - birth;
+
+if (customerEmail[0] == `@`) {
+    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ find in first place)`;
+
+} else if (customerEmail[customerEmail.length-1] == `@`) {
+    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ find in last place)`;
+
+} else if (customerEmail.includes("@")) { 
+
+} else {
+    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ not exist)` ;
+}
+
+
+
+
+document.write(firstName, lastName, customerEmail, age)
