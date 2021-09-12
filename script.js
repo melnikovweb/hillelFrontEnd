24febc1416c8761dd35da09a6b1a18e5d675f8ad
Andrew Melnikov <melnikovweb@gmail.com>
2021-09-12 12:09:26 +0300

    All done

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-firstName = prompt("Enter first name pls").trim();
+firstName = prompt("Enter first name pls").trim(); 
 lastName = prompt("Enter last name pls").trim();
 customerEmail = prompt("Enter email pls").replaceAll(' ','').toLowerCase();
 birth = prompt("Enter year of birth pls").replaceAll(' ','');
-date = new Date();
-age = date.getFullYear() - birth;
+date = new Date().getFullYear();
 
-if (customerEmail[0] == `@`) {
-    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ find in first place)`;
-
-} else if (customerEmail[customerEmail.length-1] == `@`) {
-    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ find in last place)`;
-
-} else if (customerEmail.includes("@")) { 
-
-} else {
-    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ not exist)` ;
+//Проверка даты рождения
+if (+date>birth){
+    age = date - birth;
+} else{
+    age = "Вы еще не родились"
 }
 
+//Проверка email
+if (!customerEmail.includes(`@`)) {
+    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ not exist)` ;
+} else if (customerEmail[0] == `@`) {
+    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ find in first place)`;
+} else if (customerEmail[customerEmail.length-1] == `@`) { 
+    customerEmail = `not valid email <b>${customerEmail}</b> (symbol @ find in last place)`;
+} 
 
-
-
-document.write(firstName, lastName, customerEmail, age)
+document.write(`<ul class="user">
+		<li>Name: <b>${firstName} ${lastName}</b></li>
+		<li>Email: <b>${customerEmail}</b></li>
+		<li>Age: <b>${age}</b></li>
+	</ul>`);
