3bb9f8a69b469a2739837c5352184274cf290b82
Andrew Melnikov <melnikovweb@gmail.com>
2021-09-24 20:28:26 +0300

    dz5 first commit

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
-price = 0;
-burger = prompt("Hamburger or Cheeseburger");
+// price = 0;
+// burger = prompt("Hamburger or Cheeseburger");
 
-if (burger){
-if (burger.trim().toLocaleLowerCase() == "hamburger") {
-    price += 10;
-} 
-if (burger.trim().toLocaleLowerCase() == "cheeseburger") {
-    price += 15;
-    cheese = confirm("Would you like double cheese?");
-    if(cheese){
-        price += 5;
-        cheese = " with double cheese"
-    }
-} 
-}
-potato = confirm("Would you like potato?");
+// if (burger){
+// if (burger.trim().toLocaleLowerCase() == "hamburger") {
+//     price += 10;
+// } 
+// if (burger.trim().toLocaleLowerCase() == "cheeseburger") {
+//     price += 15;
+//     cheese = confirm("Would you like double cheese?");
+//     if(cheese){
+//         price += 5;
+//         cheese = " with double cheese"
+//     }
+// } 
+// }
+// potato = confirm("Would you like potato?");
 
-if (potato) { 
-    fries = prompt("Choose fries size: small/middle/big");
-    if(fries){
-    if (fries.trim().toLocaleLowerCase() == "small") price += 10;
-    if (fries.trim().toLocaleLowerCase() == "middle") price += 15;
-    if (fries.trim().toLocaleLowerCase() == "big") price += 20;
-    }else{
-        fries = "small";
-        price += 10;
-    }
-} 
+// if (potato) { 
+//     fries = prompt("Choose fries size: small/middle/big");
+//     if(fries){
+//     if (fries.trim().toLocaleLowerCase() == "small") price += 10;
+//     if (fries.trim().toLocaleLowerCase() == "middle") price += 15;
+//     if (fries.trim().toLocaleLowerCase() == "big") price += 20;
+//     }else{
+//         fries = "small";
+//         price += 10;
+//     }
+// } 
 
-sauce = confirm("Would you like sauce?");
-if (sauce) { 
-    typeofSauce = prompt("Choose sauce: ketchup/mayonnaise");
-    if (!typeofSauce) {
-    typeofSauce = "ketchup";
-}
-    price += 10;
-} 
+// sauce = confirm("Would you like sauce?");
+// if (sauce) { 
+//     typeofSauce = prompt("Choose sauce: ketchup/mayonnaise");
+//     if (!typeofSauce) {
+//     typeofSauce = "ketchup";
+// }
+//     price += 10;
+// } 
 
-document.write(`<h2>Your order:</h2><ul>`);
+// document.write(`<h2>Your order:</h2><ul>`);
 
-if(burger) {
-document.write(`<li>Bulka 🍔: ${burger}`);
-if (burger.trim().toLocaleLowerCase() == "cheeseburger" && cheese){
-    document.write(`${cheese}</li>`);
-}else{
-    document.write(`</li>`);
-}}
-if(potato) {
-document.write(`<li>Potato 🍟: ${fries} </li>`);
-}
-if(sauce) {
-document.write(`<li>Sauce 🧂:  ${typeofSauce}</li>`);
-}
-document.write(`</ul>
-    <p>Price: ${price} </p>
-`);
+// if(burger) {
+// document.write(`<li>Bulka 🍔: ${burger}`);
+// if (burger.trim().toLocaleLowerCase() == "cheeseburger" && cheese){
+//     document.write(`${cheese}</li>`);
+// }else{
+//     document.write(`</li>`);
+// }}
+// if(potato) {
+// document.write(`<li>Potato 🍟: ${fries} </li>`);
+// }
+// if(sauce) {
+// document.write(`<li>Sauce 🧂:  ${typeofSauce}</li>`);
+// }
+// document.write(`</ul>
+//     <p>Price: ${price} </p>
+// `);
 
