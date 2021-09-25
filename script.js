2c768b42975100c826ab3b1f6d2b7bdb8373c740
Andrew Melnikov <melnikovweb@gmail.com>
2021-09-25 12:46:18 +0300

    dz5 update

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 sum=0;
-
 do{
     operator = prompt("Что хотите сделать? (+ - / *)", "+");
 }while(operator !="+" && operator !="-" && operator !="/" && operator !="*")
 
 do{
-    operand = prompt("Cколько операндов должно быть?(от 2 до 6)");
-    operand = +operand;
+    operand = +prompt("Cколько операндов должно быть?(от 2 до 6)", "3");
 }while(operand<=1 || operand>=7 || isNaN(operand))
-
 
 for (i=1; operand>= i; i++){
 
 do{
 InnerOperand = +prompt(`Введите ${i} операнд`);
-InnerOperand = +InnerOperand;
 }while(!InnerOperand || isNaN(operand))
-
 
 switch(operator){
 case "+":
