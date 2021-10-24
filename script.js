6a76326274e429655097a90775509c912bd4a10d
Andrew Melnikov <melnikovweb@gmail.com>
2021-10-24 22:57:12 +0300

    dz 12 done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 "use strict";
-debugger
 let marvelHeroes = [
 	{
 		name: "Thor"
@@ -8,7 +7,6 @@ let marvelHeroes = [
 		name: "Spider Man"
 	}
 ];
-
 let dcHeroes = [
 	{
 		name: "Superman"
@@ -20,4 +18,37 @@ let dcHeroes = [
 		name: "Deadpool"
 	}
 ];
-  
+Array.prototype.heroesRender = function(brand) {
+ let brandName = brand === "marvel" ? "marvel" : "dc";
+ document.write(`<table border=1>
+ <thead>
+   <tr>
+     <th>Name</th>
+     <th>Icon</th>
+   </tr>
+ </thead>
+ <tbody>`)
+ 
+  this.forEach(function(item) {
+    document.write(`<tr>
+     <td>${item.name}</td>
+     <td><img src="images/${brandName}/${item.name.toLowerCase().replaceAll(' ', '')}.svg">
+     </td>
+   </tr>`) 
+
+});
+document.write(`	
+</tbody>
+</table>`)
+}
+
+let brand = prompt("marvel or dc?").toLowerCase();
+while(brand !="marvel" && brand !="dc"){
+  brand = prompt(`Not (${brand}) - marvel or dc?`).toLowerCase();
+}
+
+if (brand === "marvel"){
+  marvelHeroes.heroesRender(brand);
+}else{
+  dcHeroes.heroesRender(brand);
+}
