76fd66704ff5583f0ceb5ab2613ff4527a8d3ac1
Andrew Melnikov <melnikovweb@gmail.com>
2021-11-06 19:25:14 +0200

    done

--- script.js
+++ script.js
@@ -58,3 +58,64 @@ let cosmeticsProducts = [
 		price: 300,
 	}
 ];
+
+function catName (name, category){
+	for (key in name){
+		name[key].category = category;
+	}
+}
+
+catName(kitchenProducts, "kitchen");
+catName(devicesProducts, "devices");
+catName(cosmeticsProducts, "cosmetics");
+
+let allProducts = kitchenProducts.concat(devicesProducts, cosmeticsProducts);
+
+class Product{
+	constructor(category, type, price){
+		this.category = category;
+		this.type = type;
+		this.price = price;
+	}
+	
+	render(){
+		let price= (Array.isArray(this.price))? this.price.join(`-`):this.price;
+		document.write(`<tr>
+		<td><img src="images/${this.category}/${this.type}.svg" alt="${this.type}" width="50" height="50"></td>
+		<td>${this.type}</td>
+		<td>${price} USD</td>
+	</tr>`)
+	}
+}
+
+document.write(`<table border="1">
+        <thead>
+               <tr>
+               <th>Image</th>
+               <th>Name</th>
+               <th>Price</th>
+      			</tr>
+    	</thead>
+   		 <tbody>`);
+
+		allProducts.map(prodObj=>new Product(prodObj.category, prodObj.type, prodObj.price)) //Не уверен что тут правильно каждый раз создавать новый объект, но работает без ошибок
+		.map(prodObj=>prodObj.render())
+
+document.write(` </tbody></table>`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
