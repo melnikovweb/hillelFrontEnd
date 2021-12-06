57a1fec99515afa882d072a8b2ed212210a46c1d
Andrew Melnikov <melnikovweb@gmail.com>
2021-12-06 23:15:23 +0200

    dz 16 fix 1 sec

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const block = document.querySelector('.block');
 
 setInterval(
-    ()=>{block.style.background='#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
-    block.style.left=Math.floor(Math.random() * (window.innerWidth-100)) + 'px';
-    block.style.top=Math.floor(Math.random() * (window.innerHeight-100)) + 'px';
-},
+    ()=>block.style.background='#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase(),
 500
 )
 
+setInterval(
+    ()=>{
+    block.style.left=Math.floor(Math.random() * (window.innerWidth-100)) + 'px';
+    block.style.top=Math.floor(Math.random() * (window.innerHeight-100)) + 'px';
+},
+1000
+)
