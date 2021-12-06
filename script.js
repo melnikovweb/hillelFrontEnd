c822068bb26570ddf567932e47febc43e8dd1133
Andrew Melnikov <melnikovweb@gmail.com>
2021-12-06 23:12:44 +0200

    dz16 done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// start
+const block = document.querySelector('.block');
+
+setInterval(
+    ()=>{block.style.background='#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
+    block.style.left=Math.floor(Math.random() * (window.innerWidth-100)) + 'px';
+    block.style.top=Math.floor(Math.random() * (window.innerHeight-100)) + 'px';
+},
+500
+)
+
