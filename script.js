fc217433dbe6a96f19a26cde0f4e5c5d0a720751
Andrew Melnikov <melnikovweb@gmail.com>
2021-12-20 00:35:59 +0200

    almost ready

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ const figures = [
 		background: "blue"
 	}
 ];
+
+const getEventType = (event) => console.log(`${event.type} with ${event.target.className}.`);
+
+for (let i in figures){
+
+	let innerFig = document.createElement(`div`);
+	innerFig.classList.add(figures[i].type);
+	innerFig.style.background = figures[i].background;
+	innerFig.style.height = `${figures[i].height}px`;
+	innerFig.style.width = `${figures[i].height}px`;
+    innerFig.onmouseenter = getEventType;
+    innerFig.onmouseleave = getEventType;
+
+	document.querySelector(`#figuresWrapper`).append(innerFig);
+}
+
