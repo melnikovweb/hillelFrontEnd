d8f0c2bc8f12a77d7745bbeb1a40492f0d547b61
Andrew Melnikov <melnikovweb@gmail.com>
2022-01-03 21:27:09 +0200

    dz18init

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
-const figures = [
-	{
-		type: "square",
-		width: 100,
-		height: 100,
-		background: "red"
-	},
-	{
-		type: "circle",
-		width: 150,
-		height: 150,
-		background: "green"
-	},
-	{
-		type: "rectangle",
-		width: 150,
-		height: 150,
-		background: "blue"
-	}
-];
+let block = document.querySelector(".block");
+const STEP = 10;
+// console.dir(block)
+const maxWidth = element => innerWidth - element.offsetWidth;
+const maxHeight = element => innerHeight - element.offsetHeight;
 
-const getEventType = (event) => console.log(`${event.type} with ${event.target.className}`);
 
-for (let i in figures){
-
-	let innerFig = document.createElement(`div`);
-
-	innerFig.classList.add(figures[i].type);
-
-	innerFig.style.background = figures[i].background;
-	innerFig.style.height = `${figures[i].height}px`;
-	innerFig.style.width = `${figures[i].height}px`;
-
-    innerFig.onmouseenter = getEventType;
-    innerFig.onmouseleave = getEventType;
-
-	document.querySelector(`#figuresWrapper`).append(innerFig);
+const Boom = element => {
+    let div = document.createElement(`div`);
+    div.innerText = `БЭМС`;
+    div.classList.add(`boom`);
+    element.append(div);
+    setTimeout(() => div.remove(), 2000)
 }
 
+const KEYS = {
+    37: element => {
+        if (element.offsetLeft <= 0) {
+            element.style.left = `${parseInt(element.style.left) + (STEP * 2)}px`;
+            Boom(element);
+        } else {
+             element.style.left = element.style.left === '' ? `-${STEP}px` : `${parseInt(element.style.left) - STEP}px`;
+        }
+    },
+    39: element => {
+        if (element.offsetLeft >= maxWidth(element))
+        {
+            element.style.left = `${parseInt(element.style.left) - (STEP * 2)}px`;
+            Boom(element);
+        } else {
+            element.style.left = element.style.left === '' ? `${STEP}px` : `${parseInt(element.style.left) + STEP}px`;
+        }
+    },
+    38: element => {
+        if (element.offsetTop <= 0) {
+            element.style.top = `${parseInt(element.style.top) + (STEP * 2)}px`;
+            Boom(element);
+        } else {
+            element.style.top = element.style.top === "" ? `-${STEP}px` : `${parseInt(element.style.top) - STEP}px`;
+        }
+    },
+    40: element => {
+        if (element.offsetTop >= maxHeight(element)) {
+            element.style.top = `${parseInt(element.style.top) - (STEP * 2)}px`;
+            Boom(element);
+        } else element.style.top = element.style.top === '' ? `${STEP}px` : `${parseInt(element.style.top) + STEP}px`;
+    },
+    32: element => {
+        let startPosition = element.style.top;
+        element.style.top = element.style.top === '' ? `-${STEP}px` : `${parseInt(element.style.top) - STEP}px`;
+        document.addEventListener(`keyup`, ev => {
+            if (ev.keyCode === 32) {
+                element.style.top = startPosition;
+            }
+        });
+        // setTimeout(() => element.style.top = startPosition, 500);
+    },
+
+
+    17: element => {
+        let startTop = element.offsetHeight;
+        let startWidth = element.offsetWidth;
+        element.style.height = `${startTop - (startTop * 0.25)}px`;
+        element.style.width = `${startWidth + (startWidth * 0.4)}px`;
+
+        document.addEventListener(`keyup`, ev => {
+            element.style.height = `${startTop}px`;
+            element.style.width = `${startWidth}px`;
+        })
+    }
+
+};
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 32 || event.keyCode === 17) {
+        if (!event.repeat) {
+            KEYS[event.keyCode] && KEYS[event.keyCode](block);
+        }
+    } else KEYS[event.keyCode] && KEYS[event.keyCode](block);
+
+});
